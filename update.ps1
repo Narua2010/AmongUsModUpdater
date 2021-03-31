7ab3fed70081194f4ce1421048cb0cee4df33f4d
Patrick Albus <34771575+Narua2010@users.noreply.github.com>
2021-03-31 17:21:09 +0200

    Update PS1 - Exclude com.comando.essentials.cfg

--- update.ps1
+++ update.ps1
@@ -1,5 +1,9 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem    
-            
+
+$request = 'https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest'
+$Jsontest = Invoke-RestMethod $request
+$JSONDownload = Write-Output $Jsontest.assets.browser_download_url
+
 function Unzip
 {
     param([string]$zipfile, [string]$outpath)
@@ -13,15 +17,36 @@ if (Test-Path "./mono") {
     Remove-Item "./mono" -Recurse
     
 }
+
 if(Test-Path "./BepInEx"){
-    Remove-Item "./BepInEx" -Recurse
+    if(Test-Path "./BepInEx/core"){
+        Remove-Item "./BepInEx/core" -Recurse
+    } 
+    if(Test-Path "./BepInEx/plugin"){
+        Remove-Item "./BepInEx/plugin" -Recurse
+    } 
+    if(Test-Path "./BepInEx/unity-libs"){
+        Remove-Item "./BepInEx/unity-libs" -Recurse
+    }  
+    if(Test-Path "./BepInEx"){
+        if(Test-Path "./BepInEx/cache"){
+            Remove-Item "./BepInEx/cache" -Recurse
+        } 
+        if(Test-Path "./BepInEx/patchers"){
+            Remove-Item "./BepInEx/patchers" -Recurse
+        } 
+        if(Test-Path "./BepInEx/plugins"){
+            Remove-Item "./BepInEx/plugins" -Recurse
+        }
+        if(Test-Path "./BepInEx/config"){
+            Remove-Item "./BepInEx/config/BepInEx.cfg" -Recurse
+            Remove-Item "./BepInEx/config/gg.reactor.api.cfg" -Recurse
+            Remove-Item "./BepInEx/config/me.eisbison.theotherroles.cfg" -Recurse
+        }
+    } 
 }
 
 Write-Host "`n `n `n `n Please Wait ..."
-
-$request = 'https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest'
-$Jsontest = Invoke-RestMethod $request
-$JSONDownload = Write-Output $Jsontest.assets.browser_download_url
 
 Invoke-WebRequest -Uri $JSONDownload -OutFile .\otherroles.zip
 
