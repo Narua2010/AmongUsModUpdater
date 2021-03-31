527eaab0e1ae5351cc5f61a89976fe2c74733536
Joel Weihrauch <joel.weih@web.de>
2021-03-31 19:03:28 +0200

    Error Handling, Beautify Code :)

--- update.ps1
+++ update.ps1
@@ -4,6 +4,7 @@ $request = 'https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest'
 $Jsontest = Invoke-RestMethod $request
 $JSONDownload = Write-Output $Jsontest.assets.browser_download_url
 $id = Write-Output $Jsontest.id
+$exefile = ""
 
 function Unzip
 {
@@ -11,64 +12,52 @@ function Unzip
     Expand-Archive -Path $zipfile -DestinationPath $outpath -Force
 }
  
-Write-Host "Among Us Mod Updater created by Jolle & Narua"
-
-
-if(Test-Path ".\version.txt"){
-    $idfile = Get-Content -Path .\version.txt
-    if("$($idfile)" -ne "$($id)") {
-        Write-Output $Jsontest.id | Out-File -FilePath .\version.txt
-        (get-item .\version.txt).Attributes += 'Hidden'
-        Write-Host "Old files will be deleted ..."
-        if (Test-Path "./mono") {
-            Remove-Item "./mono" -Recurse
-        }
+function ClearOtherMods 
+{
+    if (Test-Path "./mono") {
+        Remove-Item "./mono" -Recurse
+    }
+    if(Test-Path "./BepInEx"){
+        if(Test-Path "./BepInEx/core"){
+            Remove-Item "./BepInEx/core" -Recurse
+        } 
+        if(Test-Path "./BepInEx/plugin"){
+            Remove-Item "./BepInEx/plugin" -Recurse
+        } 
+        if(Test-Path "./BepInEx/unity-libs"){
+            Remove-Item "./BepInEx/unity-libs" -Recurse
+        }  
         if(Test-Path "./BepInEx"){
-            if(Test-Path "./BepInEx/core"){
-                Remove-Item "./BepInEx/core" -Recurse
+            if(Test-Path "./BepInEx/cache"){
+                Remove-Item "./BepInEx/cache" -Recurse
             } 
-            if(Test-Path "./BepInEx/plugin"){
-                Remove-Item "./BepInEx/plugin" -Recurse
+            if(Test-Path "./BepInEx/patchers"){
+                Remove-Item "./BepInEx/patchers" -Recurse
             } 
-            if(Test-Path "./BepInEx/unity-libs"){
-                Remove-Item "./BepInEx/unity-libs" -Recurse
-            }  
-            if(Test-Path "./BepInEx"){
-                if(Test-Path "./BepInEx/cache"){
-                    Remove-Item "./BepInEx/cache" -Recurse
-                } 
-                if(Test-Path "./BepInEx/patchers"){
-                    Remove-Item "./BepInEx/patchers" -Recurse
-                } 
-                if(Test-Path "./BepInEx/plugins"){
-                    Remove-Item "./BepInEx/plugins" -Recurse
+            if(Test-Path "./BepInEx/plugins"){
+                Remove-Item "./BepInEx/plugins" -Recurse
+            }
+            if(Test-Path "./BepInEx/config"){
+                if(Test-Path "./BepInEx/config/BepInEx.cfg") {
+                    Remove-Item "./BepInEx/config/BepInEx.cfg" -Recurse
                 }
-                if(Test-Path "./BepInEx/config"){
-                    if(Test-Path "./BepInEx/config/BepInEx.cfg") {
-                        Remove-Item "./BepInEx/config/BepInEx.cfg" -Recurse
-                    }
-                    if(Test-Path "./BepInEx/config/gg.reactor.api.cfg"){
-                        Remove-Item "./BepInEx/config/gg.reactor.api.cfg" -Recurse
-                    }
-                    if(Test-Path "./BepInEx/config/me.eisbison.theotherroles.cfg"){
-                        Remove-Item "./BepInEx/config/me.eisbison.theotherroles.cfg" -Recurse
-                    }
+                if(Test-Path "./BepInEx/config/gg.reactor.api.cfg"){
+                    Remove-Item "./BepInEx/config/gg.reactor.api.cfg" -Recurse
+                }
+                if(Test-Path "./BepInEx/config/me.eisbison.theotherroles.cfg"){
+                    Remove-Item "./BepInEx/config/me.eisbison.theotherroles.cfg" -Recurse
                 }
             }
-            Write-Host "`n `n `n `n Please Wait ..."
-
-            Invoke-WebRequest -Uri $JSONDownload -OutFile .\otherroles.zip
-            (get-item .\otherroles.zip).Attributes += 'Hidden'
-            Write-Host "Please Wait ..."
-
-            Unzip ".\otherroles.zip" "." 
         }
     }
-} else {
-    Write-Output $Jsontest.id | Out-File -FilePath .\version.txt
-    (get-item .\version.txt).Attributes += 'Hidden'
-    Write-Host "`n `n `n `n Please Wait ..."
+    
+}
 
+function getOtherMods {
+    ClearOtherMods
+    if(Test-Path ".\otherroles.zip"){
+        Remove-Item ".\otherroles.zip" -force
+    }
     Invoke-WebRequest -Uri $JSONDownload -OutFile .\otherroles.zip
     (get-item .\otherroles.zip).Attributes += 'Hidden'
     Write-Host "Please Wait ..."
@@ -76,7 +65,39 @@ if(Test-Path ".\version.txt"){
     Unzip ".\otherroles.zip" "." 
 }
 
-$exefile = ""
+function overwriteVersion {
+    if(Test-Path .\version.txt){
+        Remove-Item .\version.txt -force
+        Write-Output $Jsontest.id | Out-File -FilePath .\version.txt
+        (get-item .\version.txt).Attributes += 'Hidden'
+    } else {
+        Write-Output $Jsontest.id | Out-File -FilePath .\version.txt
+        (get-item .\version.txt).Attributes += 'Hidden'
+    }
+}
+
+
+Write-Host "Among Us Mod Updater created by Jolle & Narua"
+
+
+if(Test-Path ".\version.txt"){
+    $idfile = Get-Content -Path .\version.txt
+    if("$($idfile)" -ne "$($id)") {
+        overwriteVersion
+        Write-Host "Old files will be deleted ..."
+        Write-Host "`n `n `n `n `n `n `n `n  `n Please Wait ..."
+        getOtherMods 
+    } else {
+        if (((Test-Path "./mono") -eq $False) -or ((Test-Path "./BepInEx") -eq $False)) {
+            echo "Cannot find Mono or BepInEx directories ..."
+            getOtherMods
+        }
+    }
+} else {
+    overwriteVersion
+    Write-Host "`n `n `n `n `n `n `n `n  `n Please Wait ..."
+    getOtherMods
+}
 
 Try{ 
     $exefile = Get-Childitem -Path ".\Among Us.exe" -ErrorAction Stop -Include *.exe -Recurse
@@ -92,5 +113,5 @@ if("$($exefile)" -ne ""){
     Write-Host "Game is starting..."
     Start-Sleep 10
     Write-Host "After fresh update game loads slower ..."
-    Start-Sleep 10
+    Start-Sleep 25
 }
