a26f8bd15510aee2980037dc88410c2b42d5f92b
Patrick Albus <34771575+Narua2010@users.noreply.github.com>
2021-04-01 14:47:58 +0200

    Initial Gui commit

---
+++ source/GuiVersion/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace AmongUsModUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.downloadProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 108);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1288, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuHelp
+            // 
+            this.toolStripMenuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuAbout});
+            this.toolStripMenuHelp.Name = "toolStripMenuHelp";
+            this.toolStripMenuHelp.Size = new System.Drawing.Size(65, 29);
+            this.toolStripMenuHelp.Text = "Help";
+            // 
+            // toolStripMenuAbout
+            // 
+            this.toolStripMenuAbout.Name = "toolStripMenuAbout";
+            this.toolStripMenuAbout.Size = new System.Drawing.Size(164, 34);
+            this.toolStripMenuAbout.Text = "About";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.downloadProgress});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 850);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1288, 23);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // downloadProgress
+            // 
+            this.downloadProgress.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.downloadProgress.Name = "downloadProgress";
+            this.downloadProgress.Size = new System.Drawing.Size(300, 15);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1288, 873);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar downloadProgress;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuHelp;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuAbout;
+    }
+}
+
---
+++ source/GuiVersion/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.Win32;
+using System.ServiceProcess;
+using System.IO;
+
+namespace AmongUsModUpdater
+{
+    public partial class Form1 : Form
+    {
+        static string request = "https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest";
+        static int releaseId;
+        static string releaseName;
+        static string downloadUrl;
+        static string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            getOtherRoles();
+            gettingInstallationPath();
+            downloadNewVersion();
+            deleteOldFiles();
+        }
+
+       
+        private async void getOtherRoles()
+        {
+            try
+            {
+                using(HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("product", "1"));
+                    var response = await client.GetStringAsync(request);
+                    dynamic json = JsonConvert.DeserializeObject(response);
+                    downloadUrl = json.assets[0].browser_download_url;
+                    releaseName = json.name;
+                    releaseId = json.assets[0].id;
+                }
+            } 
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void downloadNewVersion()
+        {
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                    wc.DownloadFileAsync(
+                        new Uri(downloadUrl),
+                        //Path to save
+                        path + "\\" + releaseName + ".zip"
+                    );
+
+                    FileAttributes attributes = File.GetAttributes(path + "\\" + releaseName + ".zip");
+                    File.SetAttributes(path + "\\" + releaseName + ".zip", File.GetAttributes(path + "\\" + releaseName + ".zip") | FileAttributes.Hidden);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+        }
+
+        private void gettingInstallationPath()
+        {
+            try
+            {
+                var options = new EnumerationOptions()
+                {
+                    IgnoreInaccessible = true,
+                    RecurseSubdirectories = true
+                };
+
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+                foreach (var drive in allDrives)
+                {
+                    string[] dirs = Directory.GetFiles(drive.Name, @"Among Us.exe", options);
+                    if (dirs.Length > 0) path = dirs[0].Replace("\\Among Us.exe", "");
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void deleteOldFiles()
+        {
+            if (Directory.Exists(path + "\\mono"))
+            {
+                
+                //then delete folder
+                Directory.Delete(path + "\\mono", true);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            downloadProgress.Value = e.ProgressPercentage;
+        }
+    }
+}
